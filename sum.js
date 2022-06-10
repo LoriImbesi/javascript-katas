33ab29ee5e34f5fcdd19dfd8c42a4980e5944b23
Lori Imbesi <lori.imbesi@gmail.com>
2022-06-10 17:40:47 -0400

    map and nested loops

--- sum.js
+++ sum.js
@@ -1,103 +1,107 @@
 function sum(a, b) {
-    return a + b;
-  }
-  module.exports = {sum, 
-    returnTargetIndex, 
-    removeTwos, 
-    removeEvenNums, 
-    countWords, 
-    largestNumber, 
-    cheapVegetables, 
-    sumOfArray, 
-    cheaperThanTen, 
-    onlyOdds,
-    findOddValues,
-    makeObjectFromArrays,
-    changeCase}
+  return a + b;
+}
+module.exports = {
+  sum,
+  returnTargetIndex,
+  removeTwos,
+  removeEvenNums,
+  countWords,
+  largestNumber,
+  cheapVegetables,
+  sumOfArray,
+  cheaperThanTen,
+  onlyOdds,
+  findOddValues,
+  makeObjectFromArrays,
+  changeCase,
+  getOddNums,
+  arraysOfOddNumsOnly,
+};
 
 function returnTargetIndex(arrayofInts, indexToCheck) {
-if (indexToCheck >= arrayofInts.length){
-    return "Error"
-} 
-else {
-    return arrayofInts[indexToCheck]
-}
-
+  if (indexToCheck >= arrayofInts.length) {
+    return "Error";
+  } else {
+    return arrayofInts[indexToCheck];
+  }
 }
 
 function removeTwos(arrayofInts) {
-    // TODO remove all even numbers
-    // Should we rename the function?
-    // figuring even - mod % = 0
-    // Should 0 be filtered out?
-    // Do I need to update my test?
-    return arrayofInts.filter(integer => integer !== 2)
+  // TODO remove all even numbers
+  // Should we rename the function?
+  // figuring even - mod % = 0
+  // Should 0 be filtered out?
+  // Do I need to update my test?
+  return arrayofInts.filter((integer) => integer !== 2);
 }
 
 function removeEvenNums(arrayofInts, checkOddOrEven) {
-    if (checkOddOrEven == "odd") {
-
-        return (arrayofInts.filter(integer => integer %2 !== 0 || integer === 0))
-    }
-    else if (checkOddOrEven == "even") {    
-
-        return (arrayofInts.filter(integer => integer %2 == 0 && integer !== 0))
-    }
+  if (checkOddOrEven == "odd") {
+    return arrayofInts.filter((integer) => integer % 2 !== 0 || integer === 0);
+  } else if (checkOddOrEven == "even") {
+    return arrayofInts.filter((integer) => integer % 2 == 0 && integer !== 0);
+  }
 }
 
 function countWords(arrayOfWords) {
-    let longest = 0
-    arrayOfWords.forEach(str => {
-        let wordCount = str.split(' ').length
-        if (wordCount > longest){
-            longest = wordCount
-        }
-    })
-    return longest
+  let longest = 0;
+  arrayOfWords.forEach((str) => {
+    let wordCount = str.split(" ").length;
+    if (wordCount > longest) {
+      longest = wordCount;
+    }
+  });
+  return longest;
 }
 
 function largestNumber(number) {
-    // what numbers do we have
-    // convert integer into string
-    // break int in to array of strings
-    // split them?
-    // need to arrange them from largest to smallest
+  // what numbers do we have
+  // convert integer into string
+  // break int in to array of strings
+  // split them?
+  // need to arrange them from largest to smallest
 
-    return parseInt(number.toString().split('').sort((a, b) => {
-       return b - a
-    }).join(''))
+  return parseInt(
+    number
+      .toString()
+      .split("")
+      .sort((a, b) => {
+        return b - a;
+      })
+      .join("")
+  );
 }
 
 function cheapVegetables(obj) {
-    return Object.keys(obj).filter(key => obj[key] < 10)
+  return Object.keys(obj).filter((key) => obj[key] < 10);
 }
 
 function sumOfArray(strings) {
-    return strings.reduce((accumStr, nextString) => {
-        return accumStr + nextString
-    }, "")
+  return strings.reduce((accumStr, nextString) => {
+    return accumStr + nextString;
+  }, "");
 }
 
-function cheaperThanTen(arrayOfObjs){
-    return arrayOfObjs.filter(obj => obj["price"] < 10)
+function cheaperThanTen(arrayOfObjs) {
+  return arrayOfObjs.filter((obj) => obj["price"] < 10);
 }
 
-function onlyOdds(arrayofInts){
-    return arrayofInts.filter(int => int % 2 !== 0)
+function onlyOdds(arrayofInts) {
+  return arrayofInts.filter((int) => int % 2 !== 0);
 }
 
-
-function findOddValues(arrayOfObjs){
-    // let oddsOfFirstObject = onlyOdds(Object.values(arrayOfObjs[0]))
-    // console.log(oddsOfFirstObject)
-    // return oddsOfFirstObject
-    return arrayOfObjs.map(obj => onlyOdds(Object.values(obj)))
+function findOddValues(arrayOfObjs) {
+  // let oddsOfFirstObject = onlyOdds(Object.values(arrayOfObjs[0]))
+  // console.log(oddsOfFirstObject)
+  // return oddsOfFirstObject
+  return arrayOfObjs.map((obj) => onlyOdds(Object.values(obj)));
 }
 //[[3, 6, 5], [8, 2, 32], [11, 7, 9]]
 //[[3, 5], [], [11, 7, 9]]
 
-function makeObjectFromArrays(arrayOfFruit, arrayOfInts){
-    return (arrayOfFruit, arrayOfInts)
+function makeObjectFromArrays(arrayOfFruit, arrayOfInts) {
+  return arrayOfFruit, arrayOfInts;
 }
 
 // array of fruit need to now be keys
@@ -106,16 +110,15 @@ function makeObjectFromArrays(arrayOfFruit, arrayOfInts){
 // Object.assign?
 
 function changeCase(strToChange) {
-    let casedArray = strToChange.split("").map((letter, index) => {
-        if (index % 2 === 0) {
-            return letter.toLowerCase()
-        }
-        else {
-            return letter.toUpperCase()
-        }
-    })
-    
-    return casedArray.join('')
+  let casedArray = strToChange.split("").map((letter, index) => {
+    if (index % 2 === 0) {
+      return letter.toLowerCase();
+    } else {
+      return letter.toUpperCase();
+    }
+  });
+
+  return casedArray.join("");
 }
 
 // letter needs to be lower or upper
@@ -126,20 +129,21 @@ function changeCase(strToChange) {
 // upper lower upper lower
 // function will call on an empty space
 
+function getOddNums(arrayOfNums) {
+  return arrayOfNums.filter((num) => num % 2 !== 0);
+}
 
+function arraysOfOddNumsOnly(arrayOfArrays) {
+  return arrayOfArrays.map((array) => getOddNums(array));
+}
 
-
-
-
-
-
-
+// return arrays with odd numbers
+// filter an array of arrays?
+// make separate arrays first?
 
 // Emergency plan for what to do when you don't know what to do:
 // SET UP A TEST - the test will tell you what's needed - it's the
 // roadmap
-
-
 
 // accessing an array: [0] = first object
 // arrayOfObjs[0["price"] < 10]
@@ -149,25 +153,23 @@ function changeCase(strToChange) {
 // check price key for less than 10
 // filter creates new array
 
-
-
 // return arrayOfCheapVeggies
-    //let arrayOfCheapVeggies = Object.entries(obj)
+//let arrayOfCheapVeggies = Object.entries(obj)
 
 // Differences between arrays and objects
 // Arrays (or lists) are ordered - they have index value
 // Arrays can have any amount of things in ANY order
-// Objects have to have everything set in pairs of two   
+// Objects have to have everything set in pairs of two
 // One key has one value  A : 2
 // NO KEY or VALUE can exist without its pair
 // Order is not defined in an object
 
-    // else {
-    //     return (filter.evens)
-    // }
-    
- // [1, 2, 3] array
- // [0, 1, 2] indices
+// else {
+//     return (filter.evens)
+// }
+
+// [1, 2, 3] array
+// [0, 1, 2] indices
 
 // if (truthy) {
 //   this will happen
@@ -175,4 +177,3 @@ function changeCase(strToChange) {
 // if (falsey) {
 //    this will NOT happen
 // }
-
--- sum.test.js
+++ sum.test.js
@@ -1,171 +1,193 @@
-const {sum, 
-    returnTargetIndex, 
-    removeTwos, 
-    countWords, 
-    largestNumber, 
-    cheapVegetables, 
-    sumOfArray, 
-    cheaperThanTen, 
-    onlyOdds,
-    findOddValues,
-    makeObjectFromArrays,
-    changeCase} = require('./sum');
+const {
+  sum,
+  returnTargetIndex,
+  removeTwos,
+  countWords,
+  largestNumber,
+  cheapVegetables,
+  sumOfArray,
+  cheaperThanTen,
+  onlyOdds,
+  findOddValues,
+  makeObjectFromArrays,
+  changeCase,
+  getOddNums,
+  arraysOfOddNumsOnly,
+} = require("./sum");
 // only
 // skip
 
 // always use npm test
 
 describe("sum test block", () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
-    });
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 
-    test('adds 2 + 2 to equal 4', () => {
-        expect(sum(2, 2)).toBe(4);
-    });
+  test("adds 2 + 2 to equal 4", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
 
-    test('adds 3 + 2 to equal 5', () => {
-        expect(sum(3, 2)).toBe(5);
-    });
-})
+  test("adds 3 + 2 to equal 5", () => {
+    expect(sum(3, 2)).toBe(5);
+  });
+});
 
 describe("return last element from list", () => {
-    test('should return 4 from array', () => {
-        expect(returnTargetIndex([3, 2, 5, 4], 0)).toBe(3)
-    })
-    test('should return error', () => {
-        expect(returnTargetIndex([3, 2, 5, 4], 4)).toBe("Error")
-    })
-})
+  test("should return 4 from array", () => {
+    expect(returnTargetIndex([3, 2, 5, 4], 0)).toBe(3);
+  });
+  test("should return error", () => {
+    expect(returnTargetIndex([3, 2, 5, 4], 4)).toBe("Error");
+  });
+});
 
 describe("remove all instances of number 2 in array", () => {
-    test('all 2s should be removed', () => {
-        expect(removeTwos([3, 2, 5, 4])).toEqual([3, 5, 4])
-    })
-
-})
+  test("all 2s should be removed", () => {
+    expect(removeTwos([3, 2, 5, 4])).toEqual([3, 5, 4]);
+  });
+});
 
 describe("return the highest word count in an array", () => {
-    test('return the highest word count in an array', () => {
-        expect(countWords(['curious cat', 'ball', 'Aslan the lion', 'garden'])).toEqual(3)
-    })
+  test("return the highest word count in an array", () => {
+    expect(
+      countWords(["curious cat", "ball", "Aslan the lion", "garden"])
+    ).toEqual(3);
+  });
 
-    test('return the highest word count in an array', () => {
-        expect(countWords(['ball', 'flowers in the garden'])).toEqual(4)
-    })
-
-})
+  test("return the highest word count in an array", () => {
+    expect(countWords(["ball", "flowers in the garden"])).toEqual(4);
+  });
+});
 
 describe("return the largest possible number", () => {
-    test('1624 should become 6421', () => {
-        expect(largestNumber(1624)).toEqual(6421)
-    })
-
-})
+  test("1624 should become 6421", () => {
+    expect(largestNumber(1624)).toEqual(6421);
+  });
+});
 
 describe("cheap vegetables", () => {
-    test('should return everything cheaper than 10', () => {
-        expect(cheapVegetables({
-            url: "localproduce.com", lettuce: 5, tomatoes: 2, asparagus: 1969,
-        })).toEqual(["lettuce", "tomatoes"])
-    })
-})
+  test("should return everything cheaper than 10", () => {
+    expect(
+      cheapVegetables({
+        url: "localproduce.com",
+        lettuce: 5,
+        tomatoes: 2,
+        asparagus: 1969,
+      })
+    ).toEqual(["lettuce", "tomatoes"]);
+  });
+});
 
 describe("combine array of strings", () => {
-    test('return the aggregate of strings', () => {
-        expect(sumOfArray(["things", "cheese", "whiz"] )).toEqual("thingscheesewhiz")
-    })
-
-})
+  test("return the aggregate of strings", () => {
+    expect(sumOfArray(["things", "cheese", "whiz"])).toEqual(
+      "thingscheesewhiz"
+    );
+  });
+});
 
 describe("get items cheaper than 10", () => {
-    test('filters items and return ids 3 and 4', () => {
-        expect(cheaperThanTen([
-            {
-                id: 3,
-                name: 6,
-                price: 5
-            },
-            {
-                id: 8,
-                name: 2,
-                price: 32
-            },
-            {
-                id: 4,
-                name: 7,
-                price: 9
-            }
-        
-        ])).toEqual([
-            {
-                id: 3,
-                name: 6,
-                price: 5
-            },
-            {
-                id: 4,
-                name: 7,
-                price: 9
-            }
-        
-        ])
-    })
-
-})
-
+  test("filters items and return ids 3 and 4", () => {
+    expect(
+      cheaperThanTen([
+        {
+          id: 3,
+          name: 6,
+          price: 5,
+        },
+        {
+          id: 8,
+          name: 2,
+          price: 32,
+        },
+        {
+          id: 4,
+          name: 7,
+          price: 9,
+        },
+      ])
+    ).toEqual([
+      {
+        id: 3,
+        name: 6,
+        price: 5,
+      },
+      {
+        id: 4,
+        name: 7,
+        price: 9,
+      },
+    ]);
+  });
+});
 
 // func that takes array of objs and returns
 // array of objects; each obj will have same keys
 // one of the keys = price will be an int
 // return array that has all objs with price
-//lower than 10; 
+//lower than 10;
 // set a filter so you can only purchase items priced
 // less than 10
 
-
 describe("filter an array of ints and return only odds", () => {
-    test('array should return 5, 9, 15', () => {
-        expect(onlyOdds([2, 4, 5, 15, 9, 20] )).toEqual([5, 15, 9])
-    })
-
-})
+  test("array should return 5, 9, 15", () => {
+    expect(onlyOdds([2, 4, 5, 15, 9, 20])).toEqual([5, 15, 9]);
+  });
+});
 
 describe("find the odd prices in the objects", () => {
-    test('should return arrays of odd values [3, 5], [11, 7, 9]', () => {
-        expect(findOddValues([
-            {
-                id: 3,
-                name: 6,
-                price: 5
-            },
-            {
-                id: 8,
-                name: 2,
-                price: 32
-            },
-            {
-                id: 11,
-                name: 7,
-                price: 9
-            }
-        
-        ])).toEqual([[3, 5], [], [11, 7, 9]])
-    })
+  test("should return arrays of odd values [3, 5], [11, 7, 9]", () => {
+    expect(
+      findOddValues([
+        {
+          id: 3,
+          name: 6,
+          price: 5,
+        },
+        {
+          id: 8,
+          name: 2,
+          price: 32,
+        },
+        {
+          id: 11,
+          name: 7,
+          price: 9,
+        },
+      ])
+    ).toEqual([[3, 5], [], [11, 7, 9]]);
+  });
 
-// describe("create object from two arrays", () => {
-//     test('object should show fruits and numbers matched in object', () => {
-//         expect(makeObjectFromArrays(["oranges", "bananas", "coconuts"], 
-//             [5, 8, 39])).toEqual({"oranges": 5, "bananas": 8, "coconuts": 39})
-//     })
+  // describe("create object from two arrays", () => {
+  //     test('object should show fruits and numbers matched in object', () => {
+  //         expect(makeObjectFromArrays(["oranges", "bananas", "coconuts"],
+  //             [5, 8, 39])).toEqual({"oranges": 5, "bananas": 8, "coconuts": 39})
+  //     })
 
-// })
+  // })
 
-describe("taking a string and mixing the case", () => {
+  describe("taking a string and mixing the case", () => {
     test('"Abc" should return "aBc" ', () => {
-        expect(changeCase("Abc")).toEqual("aBc")
-    })
+      expect(changeCase("Abc")).toEqual("aBc");
+    });
+  });
 
-})
+  describe("take an array of numbers and return an array of odd numbers", () => {
+    test("[2, 3, 4, 7, 9] should return [3, 7, 9]", () => {
+      expect(getOddNums([2, 3, 4, 7, 9])).toEqual([3, 7, 9]);
+    });
+  });
 
-})
+  describe("take an array of arrays of integers", () => {
+    test("[[1, 2, 3], [4, 5, 6], [7, 8, 9]]should return array of arrays, only odd elements", () => {
+      expect(
+        arraysOfOddNumsOnly([
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+        ])
+      ).toEqual([[1, 3], [5], [7, 9]]);
+    });
+  });
+});
